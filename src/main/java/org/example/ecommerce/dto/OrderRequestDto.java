a9cd64db751649aa4c9d14ecc7c1b4f9695fdee9
Medo101 <medomedo1cc@gmail.com>
2026-07-20 17:38:07 +0300

    feat:add postman collection json

--- src/main/java/org/example/ecommerce/dto/OrderRequestDto.java
+++ src/main/java/org/example/ecommerce/dto/OrderRequestDto.java
@@ -2,7 +2,6 @@ package org.example.ecommerce.dto;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,8 +9,6 @@ import java.util.List;
 @Setter
 @Getter
 public class OrderRequestDto {
-    @NotNull(message = "User ID is required")
-    private Long userId;
     @NotEmpty(message = "Order must contain atleast one item")
     @Valid
     private List<OrderItemRequestDto> items;
